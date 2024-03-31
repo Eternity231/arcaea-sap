7746f99aa54014d58d0e2d9b03bca9382d5be5c9
Eternity <82715835+Eternity231@users.noreply.github.com>
2024-03-31 16:38:53 +0800

    回溯至稳定版本

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from time import time
 
-from chart import Chart
+from .chart import Chart
 from solve import solve, CoordConv
-from control import DeviceController
+from ..control import DeviceController
 
 if __name__ == '__main__':
     print('请开启游戏，测量并输入下面五个点的屏幕坐标(以"x, y"的格式输入，单位：像素)：')
@@ -14,41 +14,23 @@ if __name__ == '__main__':
     print()
     chart_path = input('谱面文件路径: ').strip()
 
-    # 询问用户是否启用延迟
-    use_delay = input('是否启用延迟？(y/n): ').strip().lower() == 'y'
-    delay = 0.0
-    if use_delay:
-        delay = float(input('请输入延时(从暂停界面重开游戏所需时间(秒))：'))
-
+    delay = float(input('延时(从暂停界面重开游戏所需时间(秒))：'))
     chart = Chart.loads(open(chart_path).read())
     conv = CoordConv(bottom_left, top_left, top_right, bottom_right)
     ans = solve(chart, conv)
     ans_iter = iter(sorted(ans.items()))
     ms, evs = next(ans_iter)
 
-    ctl = DeviceController(server_dir='.')
-    start = time() + delay if use_delay else time()  # 根据用户是否启用延迟来设置开始时间
+    ctl = DeviceController(server_dir='..')
+    ctl.tap(*retry_button)
+    start = time() + delay
     print('[client] INFO: 自动打歌已启动')
-    
-    if use_delay:
-        try:
-            ctl.tap(*retry_button)
-            while True:
-                now = round((time() - start) * 1000)
-                if now >= ms:
-                    for ev in evs:
-                        ctl.touch(*ev.pos, ev.action, pointer_id=ev.pointer)
-                    ms, evs = next(ans_iter)
-        except StopIteration:
-            print('[client] INFO: 自动打歌已终止')
-    else:
-        # 用户选择不启用延迟，等待用户按下空格键后开始自动播放
-        print('[client] INFO: 等待空格')
-        keyboard.wait('space')
-        try:
-            while True:
+    try:
+        while True:
+            now = round((time() - start) * 1000)
+            if now >= ms:
                 for ev in evs:
                     ctl.touch(*ev.pos, ev.action, pointer_id=ev.pointer)
                 ms, evs = next(ans_iter)
-        except StopIteration:
-            print('[client] INFO: 自动打歌已终止')
+    except StopIteration:
+        print('[client] INFO: 自动打歌已终止')
